135e20ba8b3112906c23713e0ab57579355e229b
Grinil Khanna and Shruti Priya <grinil+shruti@>
2017-04-04 16:28:55 -0700

    successfully save data to firebase and find and retrieve data from it

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
+// import { ALBUMS } from './mock-albums';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Injectable()
@@ -16,12 +16,18 @@ export class AlbumService {
     return this.albums;
   }
 
-  getAlbumById(albumId: number){
-    for (var i = 0; i<= ALBUMS.length-1; i++){
-      if(ALBUMS[i].id === albumId){
-        return ALBUMS[i];
-      }
-    }
+ //Save new album ....
+  addAlbum(newAlbum: Album) {
+    this.albums.push(newAlbum);
+  }
+
+  getAlbumById(albumId: string){
+      return this.angularFire.database.object('albums/' + albumId);
+  //   for (var i = 0; i<= ALBUMS.length-1; i++){
+  //     if(ALBUMS[i].id === albumId){
+  //       return ALBUMS[i];
+  //     }
+  //   }
   }
 
 }
