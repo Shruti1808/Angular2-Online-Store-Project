26df5f63f198e6e12c9d01c5545ae9adc45cefa8
Grinil Khanna and Shruti Priya <shruti@>
2017-04-04 15:26:12 -0700

    install firebase and load data into it and retrieve the data

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
-
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-marketplace',
@@ -13,8 +13,8 @@ import { AlbumService } from '../album.service';
 })
 export class MarketplaceComponent implements OnInit {
   constructor(private router: Router, private albumService: AlbumService){}
-  
-  albums: Album[];
+
+  albums: FirebaseListObservable<any[]>;
 
   ngOnInit(){
     this.albums = this.albumService.getAlbums();
